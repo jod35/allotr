e8d9930d0b550076673cf97cf65a2bbae2105883
jod35 <jodestrevin@gmail.com>
2023-08-04 19:17:31 +0300

    update departents

--- server/departments/urls.py
+++ server/departments/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.DepartmentListView.as_view(), name='department_list'),
-    path('<int:pk>/',views.DepartmentDetailView.as_view(),name='department_detail')
+    path('<int:pk>/',views.DepartmentDetailView.as_view(),name='department_detail'),
+    path('update/<int:department_id>/',views.update_department, name='department_update'),
 ]
--- server/departments/views.py
+++ server/departments/views.py
@@ -1,9 +1,14 @@
 from typing import Any, Dict
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.urls import reverse
 from django.views.generic import ListView,DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse,HttpRequest
 from .forms import DepartmentUpdateForm
 from .models import Department
+import json
 
 # Create your views here.
 
@@ -23,4 +28,29 @@ class DepartmentListView(LoginRequiredMixin,ListView):
 class DepartmentDetailView(LoginRequiredMixin,DetailView):
     template_name = 'departments/detail.html'
     queryset = Department.objects.all()
+
+
+@login_required
+def update_department(request:HttpRequest,department_id):
+    department_to_update = Department.objects.filter(pk=department_id)
+
+    print(request.is_ajax())
+
+    if request.method == "POST" and request.is_ajax():
+        data = json.loads(request.body)
+        
+
+        print(data)
+
+        department_to_update.update(
+                name = data.get('name'),
+                description = data.get('description'),
+                department_head = data.get('department_head'),
+                contact_email = data.get('contact_email')
+        )
+       
+        return JsonResponse(data={"message":"Updated Successfully"})
+
+    return JsonResponse(data={"message":"Hello from server"})
+
 
