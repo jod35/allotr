2fe46b8758cf6e83a3de99e190da5aa5e1dccd5e
jod35 <jodestrevin@gmail.com>
2023-08-05 17:45:24 +0300

    course update

--- server/courses/urls.py
+++ server/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='course_list'),
+    path('update/<int:course_id>', views.update_course, name='update_course'),
 ]
--- server/courses/views.py
+++ server/courses/views.py
@@ -1,10 +1,14 @@
 from typing import Any, Dict
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic import ListView
 from courses.models import Course
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
+from django.http import JsonResponse
 from .forms import CourseUpdateForm
+import json
+
 # Create your views here.
 
 # @login_required(login_url='/auth/login/')
@@ -22,3 +26,27 @@ class CourseListView(LoginRequiredMixin,ListView):
 
         return context
     
+@login_required
+def update_course(request:HttpRequest ,course_id):
+    course_to_update = Course.objects.filter(id= course_id)
+    if request.method == "POST" and request.is_ajax():
+        data = json.loads(request.body)    
+
+
+        course_to_update.update(
+            code = data.get('code'),
+            title = data.get('title'),
+            course_description = data.get('course_description')
+        )
+
+        return JsonResponse(data={
+            "message":"Course Updated successfully"
+        })
+    
+
+    return JsonResponse(data={
+        "message":"Hello from server"
+    })
+
+
+
