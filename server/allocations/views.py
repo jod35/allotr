828d359d721f36fce4b1091c4272b9ebd41abff7
jod35 <jodestrevin@gmail.com>
2023-08-25 20:50:30 +0300

    add allocations

--- server/allocations/views.py
+++ server/allocations/views.py
@@ -1,10 +1,19 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 
 from .models import Allocation
+from lecturers.models import LecturerCourse
 
 # Create your views here.
 
 
 class AllocationView(TemplateView):
     template_name = "allocations/index.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+
+        context['allocations'] = LecturerCourse.objects.all()
+
+        return context
