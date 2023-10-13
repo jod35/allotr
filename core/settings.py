796e3195550315c1d2ff42ddccc21d224fac5673
jod35 <jodestrevin@gmail.com>
2023-10-13 10:01:47 +0300

    change db settings

--- core/settings.py
+++ core/settings.py
@@ -134,22 +134,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {}
 
-if DEBUG:
-    DATABASES["default"] = {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
 
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('PROD_DB_URL'),
-            conn_max_age=600,
-            conn_health_checks=True,
-        ),
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('PROD_DB_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
+}
 
 
 REST_FRAMEWORK = {
