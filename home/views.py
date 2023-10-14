faf1eedc2e358466ab60422d0086f6e0dca06995
jod35 <jodestrevin@gmail.com>
2023-10-14 14:30:24 +0300

    add profile page

--- home/views.py
+++ home/views.py
@@ -39,25 +39,6 @@ def index(request):
     return render(request, "home/index.html", context)
 
 
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
-    try:
-        load_template = request.path.split("/")[-1]
-
-        if load_template == "admin":
-            return HttpResponseRedirect(reverse("admin:index"))
-        context["segment"] = load_template
-
-        html_template = loader.get_template("home/" + load_template)
-        return HttpResponse(html_template.render(context, request))
-
-    except template.TemplateDoesNotExist:
-        html_template = loader.get_template("home/page-404.html")
-        return HttpResponse(html_template.render(context, request))
-
-    except:
-        html_template = loader.get_template("home/page-500.html")
-        return HttpResponse(html_template.render(context, request))
+@login_required(login_url="/auth/login")
+def profile_page(request):
+    return render(request,'home/profile.html')
