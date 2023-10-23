5bf4c122fbed60d6d5120b4c5363723922687d85
jod35 <jodestrevin@gmail.com>
2023-10-23 06:27:03 +0300

    fix: change requirements

---
+++ samples/main.py
@@ -0,0 +1,3 @@
+from tasks import add
+result = add.delay(66, 4)
+print(result.id)
---
+++ samples/tasks.py
@@ -0,0 +1,8 @@
+from celery import Celery
+BROKER_URL = 'redis://localhost:6379/0'
+BACKEND_URL = 'redis://localhost:6379/1'
+app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL, )
+
+@app.task(name='Add two numbers')
+def add(x, y):
+    return x + y
